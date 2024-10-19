ff8ab94264cd9d3398956661fac883fd50f31535
котяра <94602071+Gamerkot911@users.noreply.github.com>
2024-10-19 17:32:29 +0300

    ready ( non-adaptive )

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,89 @@
 let createButton = document.getElementById("enter")
 let textInput = document.getElementById("textIn")
+let list = document.getElementById("list")
 
 createButton.addEventListener("click" , function(){
-    if (textInput.lenght > 0){
-        let div = document.createElement("div")
-        div.classList.add("task")
-        let checkbox = document.createElement("input")
-        checkbox.checkbox = true
-        checkbox.id = "checkbox"
-        div.appendChild(checkbox)
-        let Ntext = document.createElement("p")
-        Ntext.innerText = textInput.value
-        div.appendChild(Ntext)
-        let delBtn = document.createElement("button")
-        delBtn.style.backgroundImage = "/src/img/bin.png"
-        delBtn.id = "del"
-        div.appendChild(delBtn)
-
+    console.log("func started")
+    var betta = textInput.value
+    if (betta.length > 0){
+        let elemMain = document.createElement("div")
+        elemMain.classList.add("task");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = "checkbox";
+        elemMain.appendChild(checkbox);
+        let Ntext = document.createElement("p");
+        Ntext.innerText = textInput.value;
+        elemMain.appendChild(Ntext);
+        let delBtn = document.createElement("button");
+        delBtn.style.backgroundImage = "/src/img/bin.png";
+        delBtn.id = "del";
+        elemMain.appendChild(delBtn);
+        list.appendChild(elemMain)
         delBtn.addEventListener("click" , function(){
-            div.remove()
+            elemMain.remove();
         })
-        checkbox.addEventListener("change" , function(){
-            
+        if (textInput.value.length > 40){
+            Ntext.style.fontSize = "8px"
+        }
+        var clicked = false
+        checkbox.addEventListener("click" , function(){
+            if (clicked == false){
+                clicked = true
+                Ntext.style.textDecoration = "line-through";
+                Ntext.style.color = "rgba(0, 0, 0,0.5)"
+            }else {
+                clicked = false
+                Ntext.style.textDecoration = "none";
+                Ntext.style.color = "rgba(0, 0, 0,1)"
+            }
+            console.log(clicked)
         })
         console.log("added")
+    }
+})
+
+textInput.addEventListener("keypress" , function(keypress){
+    console.log(keypress.key)
+    if (keypress.key == "Enter") {
+        console.log("func started")
+        var betta = textInput.value
+        if (betta.length > 0){
+            let elemMain = document.createElement("div")
+            elemMain.classList.add("task");
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.id = "checkbox";
+            elemMain.appendChild(checkbox);
+            let Ntext = document.createElement("p");
+            Ntext.innerText = textInput.value;
+            elemMain.appendChild(Ntext);
+            let delBtn = document.createElement("button");
+            delBtn.style.backgroundImage = "/src/img/bin.png";
+            delBtn.id = "del";
+            elemMain.appendChild(delBtn);
+            list.appendChild(elemMain)
+            delBtn.addEventListener("click" , function(){
+                elemMain.remove();
+            })
+            if (textInput.value.length > 40){
+                Ntext.style.fontSize = "8px"
+            }
+            var clicked = false
+            checkbox.addEventListener("click" , function(){
+                if (clicked == false){
+                    clicked = true
+                    Ntext.style.textDecoration = "line-through";
+                    Ntext.style.color = "rgba(0, 0, 0,0.5)"
+                }else {
+                    clicked = false
+                    Ntext.style.textDecoration = "none";
+                    Ntext.style.color = "rgba(0, 0, 0,1)"
+                }
+                console.log(clicked)
+            })
+            console.log("added")
+            textInput.innerText = ""
+        }
     }
 })
